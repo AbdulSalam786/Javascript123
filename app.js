52386ee4253af22b24241861e25d8d303058afbf
Abdul Salam Soomro <salmankhan7778666@gmail.com>
2018-02-07 12:45:10 +0500

    This is my javascript practice

---
+++ app.js
@@ -0,0 +1,37 @@
+// alert(   "Abdul Salam is Best Android & ios App`s developer"   )
+
+// var name = "Abdul Salam"
+
+// var cast = " Soomro"
+
+// var city = " Karachi"
+
+// alert (name+cast+city);
+
+    // var a = 15;
+
+    // var b = 11;
+
+    // var c = 11;
+
+    
+    // alert (a+c+b);
+
+
+    // var cream = 75;
+
+    // var oil = 90;
+
+    // var bottle = 110;
+
+    // alert (cream+oil+bottle);
+
+    // var orgnum = 150;
+
+    // orgnum = orgnum + 10;
+
+    // alert (orgnum);
+
+    var myname = "Abdul Salam Soomro"
+
+    alert(myname);
